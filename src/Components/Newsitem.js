1115bef0ba00341ab8fa3027350947a396f7da55
itsdlhqcp <dilhaqued@gmail.com>
2023-04-24 22:26:40 +0530

    colors ui added to info cards

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -9,7 +9,7 @@ const Newsitem =(props)=>{
               <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {date}</small></p>
+                <p className="card-text"><small className="text-muted">{date}</small></p>
                 <a rel="noreferrer" href={newsUrl} target="_balnk" className="btn btn-sm btn-dark">Read More</a>
               </div>
           </div>

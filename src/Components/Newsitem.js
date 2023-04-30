3aadbaaa77673ecbc984979e09b6236aa0d5d539
itsdlhqcp <dilhaqued@gmail.com>
2023-04-30 23:31:40 +0530

    bakgd null

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Newsitem =(props)=>{
-    let {title,description,imageUrl,newsUrl,author,date,key}=props;
-    return (
-      <div key={key} className='my-3'>
-          <div className="card" style={{width: "18rem"}}>
-              <img src={imageUrl} className="card-img-top" alt="..."/>
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-muted">{date}</small></p>
-                <a rel="noreferrer" href={newsUrl} target="_balnk" className="btn btn-sm btn-dark">Read More</a>
-              </div>
-          </div>
+const Newsitem = (props) => {
+  const { title, description, imageUrl, newsUrl, author, date, key } = props;
+  const [showDescription, setShowDescription] = useState(false);
+
+  const handleReadMore = () => {
+    setShowDescription(!showDescription);
+  };
+
+  return (
+    <div key={key} className='my-3'>
+      <div className='card' style={{ width: '18rem' }}>
+        <img src={imageUrl} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>{title}</h5>
+          {showDescription && <p className='card-text'>{description}</p>}
+          <p className='card-text'>
+            <small className='text-muted'>{date}</small>
+          </p>
+          <button
+            className='btn btn-sm btn-dark'
+            onClick={handleReadMore}
+          >
+            {showDescription ? 'Read Less' : 'Read More'}
+          </button>
+          <a
+            rel='noreferrer'
+            href={newsUrl}
+            target='_blank'
+            className='btn btn-sm btn-dark ms-2'
+          >
+            Read Full Article
+          </a>
+        </div>
       </div>
-    )
-  
-}
+    </div>
+  );
+};
 
-export default Newsitem
+export default Newsitem;
